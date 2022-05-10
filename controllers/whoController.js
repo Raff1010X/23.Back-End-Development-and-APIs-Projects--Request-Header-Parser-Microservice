ad646cbd2be08b2224bc2f8d2446f6016c68a13c
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2022-05-10 08:39:01 +0200

    test commit4

--- controllers/whoController.js
+++ controllers/whoController.js
@@ -1,6 +1,8 @@
 exports.getWho = (req, res) => {
+    let ipaddress =
+        req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.json({
-        ipaddress: req.headers['Forwarded'],
+        ipaddress,
         language: req.headers['accept-language'],
         software: req.headers['user-agent'],
     });
